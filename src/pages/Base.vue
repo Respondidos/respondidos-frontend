3201b1cb3fa448caf680cedba5f5321a69c74be2
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-10-22 16:23:21 -0300

    Template fix and routes

--- src/pages/Base.vue
+++ src/pages/Base.vue
@@ -1,63 +1,49 @@
 <template>
-    <v-app id="base">
-    <!-- <v-navigation-drawer v-model="drawer" app clipped color="primary" dark>
+  <v-app id="base">
+    <v-navigation-drawer v-model="drawer" app color="indigo" dark>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
-            Application
+            Respondidos
           </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <div
-        v-for="item in items"
-          :key="item.name">
-        <v-btn block text left :to="item.link">
-        <v-list-item
-        >
-          <v-list-item-content>
-            <v-list-item-title>{{ item.name }}</v-list-item-title>
-          </v-list-item-content>
-        
-        </v-list-item>
-        </v-btn>
+      <v-list dense nav>
+        <div v-for="item in items" :key="item.name">
+          <v-btn block text left :to="item.link">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          </v-btn>
         </div>
         <v-divider></v-divider>
         <v-spacer></v-spacer>
         <v-btn @click="logout" block text>
         <v-list-item>
           <v-list-item-content>
-              
-                <v-list-item-title>Logout</v-list-item-title>
-              
-            
+            <v-list-item-title>Logout</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         </v-btn>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar dense app>
+
+    <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Respondidos</v-toolbar-title>
+    </v-app-bar>
 
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-    </v-toolbar> -->
-        <v-content fluid>
-        <transition mode="out-in">
-           <router-view></router-view>
-        </transition>
-     </v-content>
-    </v-app>
+    <v-content app fluid>
+      <transition mode="out-in">
+          <router-view></router-view>
+      </transition>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -66,9 +52,9 @@ export default {
       return {
         drawer: false,
         items: [
-          { name: 'Home', icon: 'mdi-home-city' },
-          { name: 'My Account', icon: 'mdi-account' },
-          { name: 'Users', icon: 'mdi-account-group-outline' },
+          { name: 'Home', icon: 'mdi-home-city', path: '/' },
+          { name: 'My Account', icon: 'mdi-account', path: '/' },
+          { name: 'Users', icon: 'mdi-account-group-outline', path: '/' },
         ],
         mini: true,
       }
@@ -77,7 +63,9 @@ export default {
        
     },
     methods: {
-
+      logout() {
+        this.$router.push('login')
+      }
     }
 }
 </script>
