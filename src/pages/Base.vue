b34d2550d1b58281eb58b9b59ddc18a406239b92
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-12-04 19:36:10 -0300

    RES-64: possibilitar a mudança do avatar do usuário

--- src/pages/Base.vue
+++ src/pages/Base.vue
@@ -46,9 +46,11 @@
                     <v-spacer> </v-spacer>
                     <v-list-item-avatar>
                   <img
-        src="https://cdn.vuetifyjs.com/images/john.jpg"
-        alt="John"
-      >
+                    :src="userAvatar"
+                    alt="user avatar"
+                    style="cursor: pointer;"
+                    @click="avatarDialog = !avatarDialog"
+                  >
                 </v-list-item-avatar>
                   </v-col>
                   <v-col cols="auto">
@@ -64,6 +66,54 @@
     </v-app-bar>
 
     <v-content app fluid>
+      <v-dialog
+        v-model="avatarDialog"
+        width="500"
+      >
+        <v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            Mudar avatar
+          </v-card-title>
+
+          <v-card-text>
+            <v-row>
+              <v-col 
+                v-for="option in avatarOptions"
+                :key="option"
+                cols="3"
+                :class="option === user.avatar ? 'avatar-item-selected' : 'avatar-item'"
+                @click="changeUserAvatar(option)"
+                >
+                <v-img
+                  :src="convertAvatarToUrl(option)"
+                />
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              @click="avatarDialog = false"
+            >
+              Cancelar
+            </v-btn>
+            <v-btn
+              color="primary"
+              text
+              @click="avatarDialog = false"
+            >
+              Alterar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <transition mode="out-in">
           <router-view></router-view>
       </transition>
@@ -81,7 +131,10 @@ export default {
           { name: 'Quizzes', path: '/quizzes' },
           { name: 'Questões', icon: 'mdi-account-group-outline', path: '/questoes' },
         ],
-        user: {}
+        user: {},
+        userAvatar: '',
+        avatarDialog: false,
+        avatarOptions: []
       }
     },
     async created() {
@@ -93,14 +146,62 @@ export default {
       const res = await instance.get('/user/')
       console.log("user: ", res.data)
       this.user = res.data
+      this.userAvatar = this.convertAvatarToUrl(this.user.avatar)
+      this.avatarOptions = this.getAvatarOptions()
+      console.log(this.avatarOptions)
     },
     methods: {
       logout() {
         this.$router.push('login')
+      },
+      convertAvatarToUrl(avatar) {
+        const avatarImage = require(`../assets/avatars/${avatar}.png`)
+        return avatarImage ? avatarImage : ''
+      },
+      getAvatarOptions() {
+        let options = []
+        for (let i = 1 ; i <= 151 ; i++) {
+          options.push(this.formatNumber(i, 3))
+        }
+        return options;
+      },
+      formatNumber(number, digits) {
+        let newNumber = number.toString()
+        for (let i = newNumber.length ; i < digits ; i++) {
+          newNumber = '0'.concat(newNumber)
+        }
+        return newNumber;
+      },
+      async changeUserAvatar(avatar) {
+        const instance = axios.create({
+            baseURL: 'http://localhost:8888',
+            headers: {'authorization':  `Bearer ${localStorage.token}`}
+        });
+        const res = await instance.put('/user/avatar', {
+          avatar
+        })
+        if (res.data.avatar) {
+          this.user = res.data
+          this.userAvatar = this.convertAvatarToUrl(res.data.avatar)
+          this.avatarDialog = false
+        }
       }
-    }
+    },
 }
 </script>
 
 <style scoped>
+  .avatar-item {
+    cursor: pointer;
+    opacity: 0.5;
+    filter: alpha(opacity=50);
+  }
+  .avatar-item-selected {
+    opacity: 1.0;
+    filter: alpha(opacity=100);
+  }
+  .avatar-item:hover {
+    opacity: 1.0;
+    filter: alpha(opacity=100);
+  }
 </style>
